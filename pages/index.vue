7481f6bf581a02fa1d0a0d830dc094e3f5ede223
Jorge <jorgeparraandrade@gmail.com>
2020-03-14 16:31:49 -0400

    Partners section ready

--- pages/index.vue
+++ pages/index.vue
@@ -34,6 +34,35 @@
 					<a href="#" class="btn-main-outline">Know More</a>
 				</div>
 			</div>
+			<div class="partners-section">
+				<h3>Our Partnes & Clients</h3>
+				<div class="card-deck">
+					<div class="client-card">
+						<img src="../assets/img/logo1.png" alt="client">
+						<a href="#">Learn More</a>
+					</div>
+					<div class="client-card">
+						<img src="../assets/img/logo2.png" alt="client">
+						<a href="#">Learn More</a>
+					</div>
+					<div class="client-card">
+						<img src="../assets/img/logo3.png" alt="client">
+						<a href="#">Learn More</a>
+					</div>
+					<div class="client-card">
+						<img src="../assets/img/logo4.png" alt="client">
+						<a href="#">Learn More</a>
+					</div>
+					<div class="client-card">
+						<img src="../assets/img/logo5.png" alt="client">
+						<a href="#">Learn More</a>
+					</div>
+					<div class="client-card">
+						<img src="../assets/img/k.png" alt="client">
+						<a href="#">Learn More</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -75,10 +104,75 @@ export default {
 	}
 }
 
-.image-container{
+.image-container {
 	background-size: contain;
 	background-position: center;
 	background-repeat: no-repeat;
 	width: 100%;
+}
+
+.partners-section {
+
+	h3{
+		text-align: center;
+    font-size: 45px;
+    margin-top: 70px;
+    font-family: montserrat;
+	}
+
+	.card-deck {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+
+		.client-card {
+			width: 30%;
+			background-color: white;
+			padding: 0;
+			min-height: 360px;
+			border-radius: 5px;
+			margin: 20px;
+			display: flex;
+			align-items: center;
+			justify-content: space-evenly;
+			flex-direction: column;
+			box-shadow: 0px 10px 30px #99999933;
+
+			&:nth-child(1) {
+				margin-top: 120px;
+			}	
+			&:nth-child(3) {
+				margin-top: 120px;
+			}	
+			&:nth-child(5) {
+				margin-top: -80px;
+			}	
+
+			&:hover {
+				background-color: #185f7f;
+
+				a {
+					color: #fff;
+					transform: translateY(5px) scale(1.1);
+				}
+
+				img {
+					filter: brightness(0) invert(1);
+				}
+			}
+
+			a {
+				color: #185f7f;
+				text-decoration: none;
+				font-weight: 500;
+				transition: all ease 0.3s;
+			}
+
+			img {
+				max-width: 100%;
+			}
+		}
+	}
 }
 </style>
